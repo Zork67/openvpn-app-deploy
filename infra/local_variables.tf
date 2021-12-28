6109b336b29fc876dfd1e96794e67a2664233753
IvaKuzmin <ivakuzmin@yotateam.com>
2021-12-29 02:39:19 +0300

    scale dev to 0

--- infra/local_variables.tf
+++ infra/local_variables.tf
@@ -1,6 +1,6 @@
 locals {
   prod_instance      = 0
-  dev_instance       = 1
+  dev_instance       = 0
   image_name         = "ubuntu-20-04-x64"
   availability_zone  = "lon1"
   prod_metadata_tags = ["prod", "vpn"]
