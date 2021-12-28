2e0c29ab27365c40097e230aa8d08a8fe2696ea2
IvaKuzmin <ivakuzmin@yotateam.com>
2021-12-29 02:02:57 +0300

    add module checks

--- infra/local_variables.tf
+++ infra/local_variables.tf
@@ -1,6 +1,6 @@
 locals {
-  prod_instance      = 1
-  dev_instance       = 0
+  prod_instance      = 0
+  dev_instance       = 1
   image_name         = "openvpn-18-04"
   availability_zone  = "lon1"
   prod_metadata_tags = ["prod", "vpn"]
--- tf-modules/tf-digitalocean-instance/main.tf
+++ tf-modules/tf-digitalocean-instance/main.tf
@@ -1,9 +1,29 @@
-data "digitalocean_image" "main" {
-  slug = var.image_name
+data "digitalocean_images" "main" {
+  filter {
+    key    = "distribution"
+    values = [var.image_name]
+  }
+  filter {
+    key    = "regions"
+    values = [var.region_slug]
+  }
+  sort {
+    key       = "created"
+    direction = "desc"
+  }
 }
 
-data "digitalocean_region" "main" {
-  slug = var.region_slug
+data "digitalocean_regions" "available" {
+  filter {
+    key    = "available"
+    values = ["var.region_slug"]
+  }
+
+  sort {
+    key       = "name"
+    direction = "desc"
+  }
+
 }
 
 data "digitalocean_sizes" "main" {
@@ -20,9 +40,9 @@ data "digitalocean_sizes" "main" {
 
 resource "digitalocean_droplet" "vm" {
   count  = var.instances
-  image  = data.digitalocean_image.main.id
+  image  = element(data.digitalocean_images.main.images, 0).slug
   name   = "${var.hostname_prefix}-${var.region_slug}-${count.index + 1}.${var.dns_suffix}"
-  region = data.digitalocean_region.main.name
+  region = element(data.digitalocean_regions.available.regions, 0).slug
   size   = element(data.digitalocean_sizes.main.sizes, 0).slug
   tags   = var.tags
 }
