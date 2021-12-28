fb148b5a11240f3ed85a72853600bfc9c541ea6c
IvaKuzmin <ivakuzmin@yotateam.com>
2021-12-29 01:37:28 +0300

    fix digitalocean_image

--- tf-modules/tf-digitalocean-instance/main.tf
+++ tf-modules/tf-digitalocean-instance/main.tf
@@ -20,7 +20,7 @@ data "digitalocean_sizes" "main" {
 
 resource "digitalocean_droplet" "vm" {
   count  = var.instances
-  image  = data.digitalocean_image.main.name
+  image  = data.digitalocean_image.main.id
   name   = "${var.hostname_prefix}-${var.region_slug}-${count.index + 1}.${var.dns_suffix}"
   region = data.digitalocean_region.main.name
   size   = element(data.digitalocean_sizes.main.sizes, 0).slug
